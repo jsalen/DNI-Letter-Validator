d4e1c5864f16e29f770656770b269bb50512a79b
Joseph Salen <jsalen.91@gmail.com>
2021-02-06 13:13:09 -0400

    fixed functionality when calling LETRAS[letraDNI(numerosDNI)]

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const MODULO_DNI = 23;
 let numerosDNI = 0;
-const letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
+const LETRAS = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
 
-numerosDNI = 25421542;
+numerosDNI = parseInt(prompt("Inserte numero de DNI"));
 
-function isNumber (DNI) {
-    return typeof DNI == "number";
-}
-
-// Validar que sea un numero y que sea un numero igual a 8.
-while (!(isNumber(numerosDNI)) || (!(numerosDNI.toString().length == 8))) {
-    numerosDNI = prompt(`Error: 
+// Validar que sea un numero y que sean 8 digitos.
+while ((numerosDNI.toString().length !== 8) || !(Number.isInteger(numerosDNI))) {
+    numerosDNI = parseInt(prompt(`Error:
     1.- DNI debe ser solo numeros.
     2.- DNI debe contener 8 digitos.
-    Reintente de nuevo`);
+    Reintente de nuevo`));
 }
 
 const letraDNI = numero => numero % MODULO_DNI;
-console.log(letras[letraDNI]);
 
-console.log(`Su DNI es: ${numerosDNI}-${letras[letraDNI]}`);
+console.log(`Su DNI es: ${numerosDNI}-${LETRAS[letraDNI(numerosDNI)]}`);
